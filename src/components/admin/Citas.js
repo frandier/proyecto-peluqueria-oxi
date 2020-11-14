54fce931640aafbe541f2d56e6ebf694fabc929e
Fabian <fabian@stackverse.com>
2020-11-14 16:32:42 -0500

    add export to excel file option

--- src/components/admin/Citas.js
+++ src/components/admin/Citas.js
@@ -3,8 +3,13 @@ import { useDispatch, useSelector } from 'react-redux'
 import { startDeleting, startLoadingCitas } from '../../actions/citas';
 import NavbarAdmin from './NavbarAdmin'
 
-import moment from 'moment'
-import 'moment/locale/es-mx'
+import ExportExcel from 'react-export-excel';
+import moment from 'moment';
+import 'moment/locale/es-mx';
+
+const ExcelFile = ExportExcel.ExcelFile;
+const ExcelSheet = ExportExcel.ExcelSheet;
+const ExcelColumn = ExportExcel.ExcelColumn;
 
 export default function Citas() {
 
@@ -43,7 +48,18 @@ export default function Citas() {
             <NavbarAdmin/>
             <div className="row">
                 <div className="col-md-7 mx-auto">
-                    <table className="table table-striped mt-5">
+                    <div className="mt-2">
+                        <ExcelFile element={<button className="btn btn-primary">Exportar a Excel</button>} filename="Citas a atender">
+                            <ExcelSheet data={citas} name="Citas">
+                                <ExcelColumn label="Id" value="id"/>
+                                <ExcelColumn label="Servicio" value="servicio"/>
+                                <ExcelColumn label="Fecha" value="fecha"/>
+                                <ExcelColumn label="Empleado" value="empleado"/>
+                                <ExcelColumn label="Cliente" value="cliente"/>
+                            </ExcelSheet>
+                        </ExcelFile>
+                    </div>
+                    <table className="table table-striped mt-3">
                         <thead className="thead-dark">
                             <tr>
                                 <th>Servicio</th>
